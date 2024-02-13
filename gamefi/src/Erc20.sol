41faa607ce8bcb793a133f22454f5a49160eb152
goodness <goodnesskolapo>
2024-02-13 02:58:51 +0100

    reward nft and token built

---
+++ gamefi/src/Erc20.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+ 
+ 
+    constructor(address _initialowner) ERC20("MyToken", "MTK") Ownable(_initialowner) {
+        _mint(msg.sender, 1000000 ether);
+    }
+
+
+}
